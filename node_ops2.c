dc49affa0df97b808458d2c855e57dffc9c91a3d
JOSH <omojolajoshua@gmail.com>
2022-12-18 22:50:21 +0000

    add

--- node_ops2.c
+++ node_ops2.c
@@ -23,7 +23,7 @@ void add_nodes(stack_t **stack, unsigned int line_number)
 stack_t *temp;
 	if (len_dlistint(*stack) < 2)
 	{
-		dprintf(2, "L%u: can't swap, stack too short\n", line_number);
+		dprintf(2, "L%u: can't add, stack too short\n", line_number);
 		free_list(*stack);
 		free(global_t.ptr_line);
 		exit(EXIT_FAILURE);
